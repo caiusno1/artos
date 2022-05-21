ebcec85b33473641b68a7ec8a54c3027728ec8e6
Kai Biermeier <kbiermeier1@web.de>
2022-05-21 23:48:46 +0200

    change datatype length of result row item in trial result

--- artos-api/DataStructure/DB_Result.ts
+++ artos-api/DataStructure/DB_Result.ts
@@ -9,7 +9,7 @@ export class DB_Result implements IResult{
     public ID!: number;
     @Column()
     public timestamp!: number;
-    @Column("longtext")
+    @Column("varchar", { length: 5000 })
     public result!: string;
     @Column()
     public participant_id!: string;
