2e86515d2b187213d3b5a87e5d76c672f3e3c6b2
Kai Biermeier <kai.biermeier@uni-paderborn.de>
2022-07-15 10:07:59 +0200

    fix participant service targets wrong url

--- server/artos-frontend/src/app/participantService/participant.service.ts
+++ server/artos-frontend/src/app/participantService/participant.service.ts
@@ -9,12 +9,12 @@ export class ParticipantService {
 
   constructor(private http: HttpClient) { }
   getParticipantID(expermimentID: number){
-    return this.http.get<DB_Participant>(`http://localhost:8000/participant/${expermimentID}`
+    return this.http.get<DB_Participant>(`https://artosapi.kai-biermeier.de/participant/${expermimentID}`
     ).toPromise().then((data) => {return data.name})
   }
   updateParticipant(experimentID: number, currentParticipant: string){
 
     const participant: Partial<DB_Participant> = {experiment: {ID: experimentID}, name:currentParticipant}
-    return this.http.post<void>(`http://localhost:8000/participant/${experimentID}`, participant).subscribe((res) =>{})
+    return this.http.post<void>(`https://artosapi.kai-biermeier.de/participant/${experimentID}`, participant).subscribe((res) =>{})
   }
 }
